bc961c86c93bea4d1580daf619dc7ec9ba10a679
kittithat <tortong9191@gmail.com>
2024-12-18 13:23:42 +0700

    update user.php api in date 2024-12-18

---
+++ services/api/users/user.php
@@ -0,0 +1,17 @@
+<?php
+	session_start();
+	require_once dirname(dirname(__DIR__))."/config/Connection.php";
+	if(!empty($_SESSION['CSRF_TOKEN'])){
+		$csrf_token = $_SESSION['CSRF_TOKEN'];
+	}
+	$db = Connection::getInstance();
+	$username = isset($_POST['username']) ? filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : '';
+	$password = isset($_POST['password']) ? filter_var($_POST['password'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : '';
+	$is_active = isset($_POST['is_active']) ? filter_var($_POST['is_active'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : 'N';
+	$created_at = date('Y-m-d H:i:s');
+	//isset($_POST['username']) ? filter_var($_POST['username'], FILTER_SANITIZE_FULL_SPECIAL_CHARS) : '';
+	
+	$sql = "insert into users (username, password, is_active, created_at) values (?,?,?,?)";
+	$stmt = $db->db->prepare($sql);
+	$stmt->execute([$username, $password, $is_active, $created_at]);
+?>
